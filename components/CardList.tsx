75b543d6cbd829f43ca4c9d73d9393fcf4ced6e2
leonardo <leonardocamelo20@gmail.com>
2025-01-06 00:29:28 -0300

    :sparkles: feat: create a new grid for travels and use a Fetch from fetchind datas

--- components/CardList.tsx
+++ components/CardList.tsx
@@ -1,43 +1,67 @@
-import { FlatList, StyleSheet, View } from "react-native";
-import Card from "./Card";
+import { Text, StyleSheet, View, Dimensions } from "react-native";
+import { useState, useEffect } from "react";
 
-interface Item {
+const { width } = Dimensions.get("window");
+const item = (width - 60) / 2;
+
+type Expense = {
+  spentTitle: string;
+  category: string;
+  spentValue: number;
+  NumberOfDays: number;
+};
+
+type Travel = {
   id: number;
-  name: string;
-}
+  title: string;
+  expenses: Expense[];
+};
 
-const archives: Item[] = [
-  { id: 1, name: "Acapulco" },
-  { id: 2, name: "Maldivas" },
-  { id: 3, name: "Viagem a trabalho" },
-];
+type TravelsData = {
+  travels: Travel[];
+};
 
 export default function CardList() {
-  const numColumn = 2;
+  const [travel, setTravel] = useState([]);
+
+  useEffect(() => {
+    fetch("https://leonardocaml.github.io/travelingbay_api/travels.json")
+      .then((response) => response.json())
+      .then((data) => setTravel(data.travel))
+      .catch((error) => console.error("Erro ao buscar os dados:", error));
+  }, []);
 
   return (
-    <FlatList
-      data={archives}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={Card}
-      style={styles.list}
-      numColumns={numColumn}
-    />
+    <View style={styles.list}>
+      {travel.map((travel) => (
+        <View key={travel.id} style={[styles.col, { width: item }]}>
+          <View style={styles.card}></View>
+          <Text style={styles.cardTitle}>{travel.title}</Text>
+        </View>
+      ))}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  list: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+  },
   card: {
-    width: 130,
-    height: 130,
+    width: 160,
+    height: 160,
     backgroundColor: "#333",
     borderRadius: 10,
-    margin: 5,
   },
   cardTitle: {
-    fontSize: 20,
+    fontSize: 15,
+    width: 150,
+    fontFamily: "Poppins-Bold",
   },
-  list: {
-    flex: 1,
+  col: {
+    alignItems: "center",
+    height: 220,
   },
 });
